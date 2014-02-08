3c69c58884a3620dfa0225724c5da12673eb7cf8
= <=>
2014-02-08 07:57:12 -0500

    session

---
+++ Controller/session.php
@@ -0,0 +1,57 @@
+<?php
+
+
+require 'fb/facebook.php';
+
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+		'appId'  => '647770695269383',
+		'secret' => '02eb0ced79dc99a8a7fa1fb7e80cafe7',
+));
+
+$db=  new DB();
+
+
+// Get User ID
+$user = $facebook->getUser();
+
+// We may or may not have this data based on whether the user is logged in.
+//
+// If we have a $user id here, it means we know the user is logged into
+// Facebook, but we don't know if the access token is valid. An access
+// token is invalid if the user logged out of Facebook.
+
+
+
+
+
+if ($user) {
+	try {
+		// Proceed knowing you have a logged in user who's authenticated.
+		$user_profile = $facebook->api('/me');
+		$db->addUser($user_profile['id'], $user_profile['first_name'], $user_profile['last_name'], $user_profile['email']);
+
+
+
+		//add user to DB if new
+
+
+	} catch (FacebookApiException $e) {
+		error_log($e);
+		$user = null;
+	}
+}
+
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+	$logoutUrl = $facebook->getLogoutUrl();
+	header( 'Location: http://www.yoursite.com/new_page.html' ) ;
+} else {
+	$statusUrl = $facebook->getLoginStatusUrl();
+	$loginUrl = $facebook->getLoginUrl();
+}
+
+
+?>
+
+?>
